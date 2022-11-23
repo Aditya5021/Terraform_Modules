c3e4c93817dc98a367db75c89a34027fc04b0652
Aditya Chandrakar <adityachandrakar@C02FN48HQ6LR.local>
2022-11-23 15:10:52 +0530

    Added README.md for the complete AWS folder and created the sample terraform provider file

---
+++ AWS/README.md
@@ -0,0 +1,3 @@
+This folder has been created to contain terraform modules and resources for provisioning resources in AWS.
+
+All the resources present here are created with Terraform version 1.3.5 and AWS provider version 4.40.0
---
+++ AWS/terraform_provider.tf
@@ -0,0 +1,15 @@
+#This is an example for terraform provider that should be added just before modules in the modules file or can be copied to the same folder as the module main.tf file
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "ap-south-1"
+}
